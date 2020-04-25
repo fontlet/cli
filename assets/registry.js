255e4d901cfd82dca35f2387d6f1b1d02e912c27
thinkholic <mailtokasun@gmail.com>
2020-04-26 01:34:10 +0530

    Add assets/registry.js

---
+++ assets/registry.js
@@ -0,0 +1,206 @@
+module.exports = [
+    {
+        "id": "1",
+        "familyName": "Yaldevi",
+        "description": "",
+        "license": "OFL",
+        "designers": [],
+        "version": "1.060",
+        "foundry": "mooniak",
+        "issueTrackerURL": "",
+        "srcURL": "",
+        "coverImageUrl": "https://raw.githubusercontent.com/fontlet/assets/master/resources/covers/yaldevi.svg?sanitize=true",
+        "fontStyles": [
+            {
+                "styleName": "Regular",
+                "fontUrl": "https://github.com/mooniak/yaldevi-fonts/raw/master/fonts/ttf/Yaldevi-Regular.ttf"
+            },
+            {
+                "styleName": "Bold",
+                "fontUrl": "https://github.com/mooniak/yaldevi-fonts/raw/master/fonts/ttf/Yaldevi-Bold.ttf"
+            },
+            {
+                "styleName": "ExtraBold",
+                "fontUrl": "https://github.com/mooniak/yaldevi-fonts/raw/master/fonts/ttf/Yaldevi-ExtraBold.ttf"
+            },
+            {
+                "styleName": "Medium",
+                "fontUrl": "https://github.com/mooniak/yaldevi-fonts/raw/master/fonts/ttf/Yaldevi-Medium.ttf"
+            },
+            {
+                "styleName": "SemiBold",
+                "fontUrl": "https://github.com/mooniak/yaldevi-fonts/raw/master/fonts/ttf/Yaldevi-SemiBold.ttf"
+            }
+        ],
+        "tags": [],
+        "free": true,
+        "publishedAt": "Sun Oct 28 2018 12:47:40 GMT+0530",
+        "updatedAt": "Sun Oct 28 2018 12:47:40 GMT+0530"
+    },
+    {
+        "id": "2",
+        "familyName": "Post No Bills",
+        "description": "",
+        "license": "OFL",
+        "designers": [],
+        "version": "1.220",
+        "foundry": "mooniak",
+        "issueTrackerURL": "",
+        "srcURL": "",
+        "coverImageUrl": "https://raw.githubusercontent.com/fontlet/assets/master/resources/covers/post-no-bills.svg?sanitize=true",
+        "fontStyles": [
+            {
+                "styleName": "Light",
+                "fontUrl": "https://github.com/mooniak/post-no-bills-font/raw/dev/fonts/ttf/StickNoBills-Light.ttf"
+            },
+            {
+                "styleName": "Regular",
+                "fontUrl": "https://github.com/mooniak/post-no-bills-font/raw/dev/fonts/ttf/StickNoBills-Regular.ttf"
+            },
+            {
+                "styleName": "Medium",
+                "fontUrl": "https://github.com/mooniak/post-no-bills-font/raw/dev/fonts/ttf/StickNoBills-Medium.ttf"
+            },
+            {
+                "styleName": "SemiBold",
+                "fontUrl": "https://github.com/mooniak/post-no-bills-font/raw/dev/fonts/ttf/StickNoBills-SemiBold.ttf"
+            },
+            {
+                "styleName": "Bold",
+                "fontUrl": "https://github.com/mooniak/post-no-bills-font/raw/dev/fonts/ttf/StickNoBills-Bold.ttf"
+            },
+            {
+                "styleName": "ExtraBold",
+                "fontUrl": "https://github.com/mooniak/post-no-bills-font/raw/dev/fonts/ttf/StickNoBills-ExtraBold.ttf"
+            }
+        ],
+        "tags": [],
+        "free": true,
+        "publishedAt": "Sun Oct 28 2018 12:47:40 GMT+0530",
+        "updatedAt": "Sun Oct 28 2018 12:47:40 GMT+0530"
+    },
+    {
+        "id": "3",
+        "familyName": "Abhaya Libre",
+        "description": "",
+        "license": "OFL",
+        "designers": [],
+        "version": "1.060",
+        "foundry": "mooniak",
+        "issueTrackerURL": "",
+        "srcURL": "",
+        "coverImageUrl": "https://raw.githubusercontent.com/fontlet/assets/master/resources/covers/abhaya-libre.svg?sanitize=true",
+        "fontStyles": [
+            {
+                "styleName": "Regular",
+                "fontUrl": "https://github.com/mooniak/abhaya-libre-font/raw/master/fonts/ttf/AbhayaLibre-Regular.ttf"
+            },
+            {
+                "styleName": "Bold",
+                "fontUrl": "https://github.com/mooniak/abhaya-libre-font/raw/master/fonts/ttf/AbhayaLibre-Bold.ttf"
+            },
+            {
+                "styleName": "ExtraBold",
+                "fontUrl": "https://github.com/mooniak/abhaya-libre-font/raw/master/fonts/ttf/AbhayaLibre-ExtraBold.ttf"
+            },
+            {
+                "styleName": "Medium",
+                "fontUrl": "https://github.com/mooniak/abhaya-libre-font/raw/master/fonts/ttf/AbhayaLibre-Medium.ttf"
+            },
+            {
+                "styleName": "SemiBold",
+                "fontUrl": "https://github.com/mooniak/abhaya-libre-font/raw/master/fonts/ttf/AbhayaLibre-SemiBold.ttf"
+            }
+        ],
+        "tags": [],
+        "free": true,
+        "publishedAt": "Sun Oct 28 2018 12:47:40 GMT+0530",
+        "updatedAt": "Sun Oct 28 2018 12:47:40 GMT+0530"
+    },
+    {
+        "id": "4",
+        "familyName": "GalleALPHA",
+        "description": "",
+        "license": "OFL",
+        "designers": [],
+        "version": "0.050; alpha",
+        "foundry": "mooniak",
+        "issueTrackerURL": "",
+        "srcURL": "",
+        "coverImageUrl": "https://raw.githubusercontent.com/fontlet/assets/master/resources/covers/galle-alpha.svg?sanitize=true",
+        "fontStyles": [
+            {
+                "styleName": "Regular",
+                "fontUrl": "https://github.com/mooniak/galle-fonts/raw/ALPHA/fonts/ttf/GalleALPHA-Regular.ttf"
+            }
+        ],
+        "tags": [],
+        "free": true,
+        "publishedAt": "Sun Oct 28 2018 12:47:40 GMT+0530",
+        "updatedAt": "Sun Oct 28 2018 12:47:40 GMT+0530"
+    },
+    {
+        "id": "5",
+        "familyName": "Gemunu Libre",
+        "description": "",
+        "license": "OFL",
+        "designers": [],
+        "version": "1.001",
+        "foundry": "mooniak",
+        "issueTrackerURL": "",
+        "srcURL": "",
+        "coverImageUrl": "https://raw.githubusercontent.com/fontlet/assets/master/resources/covers/gemunu-libre.svg?sanitize=true",
+        "fontStyles": [
+            {
+                "styleName": "Light",
+                "fontUrl": "https://github.com/mooniak/gemunu-libre-font/raw/master/fonts/ttf/GemunuLibre-Light.ttf"
+            },
+            {
+                "styleName": "Regular",
+                "fontUrl": "https://github.com/mooniak/gemunu-libre-font/raw/master/fonts/ttf/GemunuLibre-Regular.ttf"
+            },
+            {
+                "styleName": "Medium",
+                "fontUrl": "https://github.com/mooniak/gemunu-libre-font/raw/master/fonts/ttf/GemunuLibre-Mediu.ttf"
+            },
+            {
+                "styleName": "SemiBold",
+                "fontUrl": "https://github.com/mooniak/gemunu-libre-font/raw/master/fonts/ttf/GemunuLibre-SemiBold.ttf"
+            },
+            {
+                "styleName": "Bold",
+                "fontUrl": "https://github.com/mooniak/gemunu-libre-font/raw/master/fonts/ttf/GemunuLibre-Bold.ttf"
+            },
+            {
+                "styleName": "ExtraBold",
+                "fontUrl": "https://github.com/mooniak/gemunu-libre-font/raw/master/fonts/ttf/GemunuLibre-ExtraBold.ttf"
+            }
+        ],
+        "tags": [],
+        "free": true,
+        "publishedAt": "Sun Oct 28 2018 12:47:40 GMT+0530",
+        "updatedAt": "Sun Oct 28 2018 12:47:40 GMT+0530"
+    },
+    {
+        "id": "6",
+        "familyName": "KaputaALPHA",
+        "description": "",
+        "license": "GPL-3.0-or-later",
+        "designers": [],
+        "version": "2.910; alpha",
+        "foundry": "mooniak",
+        "issueTrackerURL": "",
+        "srcURL": "",
+        "coverImageUrl": "https://raw.githubusercontent.com/fontlet/assets/master/resources/covers/kaputaALPHA.svg?sanitize=true",
+        "fontStyles": [
+            {
+                "styleName": "Regular",
+                "fontUrl": "https://github.com/mooniak/kaputa-font/raw/dev/fonts/ttf/Kaputa-Regular.ttf"
+            }
+        ],
+        "tags": [],
+        "free": false,
+        "publishedAt": "Sun Oct 28 2018 12:47:40 GMT+0530",
+        "updatedAt": "Sun Oct 28 2018 12:47:40 GMT+0530"
+    }
+];
